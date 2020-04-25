4cd8e6db21c939a3ccf4525d3f8659fc8479372a
Florian DELRIEU <47951736+Florian-DELRIEU@users.noreply.github.com>
2020-04-25 20:12:02 +0200

    [Major Update] -- System.py imported from desktop

---
+++ System.py
@@ -0,0 +1,35 @@
+from SystemGen import *
+import numpy as np
+
+def Test1(howmany=10):
+	"""
+	Test si les orbites sont consistantes
+	:return:
+	"""
+	n = 0
+	while n <= howmany:
+		S = System()
+		print(S)
+		S.clearorbit()
+		for starindic in S.Star:
+			curstar = getattr(S,starindic)
+			if len(curstar.OrbitList) is not curstar.NbOrbit:
+				print("Error")
+				break
+	n += 1
+
+def TestDetermineDistance(howmany=10):
+	error = 0
+	for i in np.arange(howmany):
+		try:
+			DetermineDistance("Terrestrial")
+		except:
+			error += 1
+	print(str(error) + " errors")
+
+def UserSystem():
+	S = System(auto=False)
+	S.addAllStar()
+	S.clearorbit()
+	S.Show()
+	return S
